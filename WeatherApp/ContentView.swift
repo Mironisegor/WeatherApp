00ad96fa22de1aaa6127d4b6f53242344beab0f4
Mironisegor <mironove55@icloud.com>
2024-04-03 16:30:51 +0300

    new commit

--- WeatherApp/ContentView.swift
+++ WeatherApp/ContentView.swift
@@ -8,25 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isNight = false
     var body: some View {
         ZStack {
-            LinearGradient(colors: [.blue, .white], startPoint: .top, endPoint: .bottomTrailing)
-                .ignoresSafeArea(.all)
+            BackgroundView(topColor: isNight ? .black: .blue, bottomColor: isNight ? .gray: .white)
             VStack {
-                Text("Pyatigorsk, RUS")
-                    .font(.system(size: 32, weight: .medium, design: .default))
-                    .foregroundColor(.white)
-                    .padding()
-                VStack {
-                    Image(systemName: "cloud.sun.rain.fill")
-                        .renderingMode(.original)
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 180, height: 180)
-                    Text("36°")
-                        .font(.system(size: 70, weight: .medium, design: .default))
-                        .foregroundColor(.white)
+                CityTextView(cityName: "Pyatigorsk, RUS")
+                MainWeatherStatusView(imageName: "cloud.sun.fill", temperature: 36)
+                    .padding(.bottom, 40)
+                HStack {
+                    weatherDays(temperature: 15, imageName: "sun.max.fill", dayweek: "Mon")
+                    weatherDays(temperature: 30, imageName: "cloud.sun.fill", dayweek: "Tue")
+                    weatherDays(temperature: 35, imageName: "sun.max.fill", dayweek: "Wed")
+                    weatherDays(temperature: 20, imageName: "cloud.sun.rain.fill", dayweek: "Thu")
+                    weatherDays(temperature: 25, imageName: "sun.rain.fill", dayweek: "Fri")
                 }
+                ButtonView(title: "Change theme", textColor: .blue, backgroundColor: .white)
                 Spacer()
             }
         }
@@ -35,4 +32,59 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+}
+
+struct weatherDays: View {
+    var temperature: Int
+    var imageName: String
+    var dayweek: String
+    var body: some View {
+        VStack {
+            Text("\(dayweek)")
+                .font(.system(size: 15, weight: .medium, design: .default))
+                .foregroundColor(.white)
+            Image(systemName: "\(imageName)")
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 60, height: 60)
+            Text("\(temperature)°")
+                .font(.system(size: 30, weight: .medium, design: .default))
+                .foregroundColor(.white)
+        }
+    }
+}
+
+struct BackgroundView: View {
+    var topColor: Color
+    var bottomColor: Color
+    var body: some View {
+        LinearGradient(colors: [.blue, .white], startPoint: .top, endPoint: .bottomTrailing)
+            .ignoresSafeArea(.all)
+    }
+}
+
+struct CityTextView: View {
+    var cityName: String
+    var body: some View {
+        Text(cityName)
+            .font(.system(size: 32, weight: .medium, design: .default))
+            .foregroundColor(.white)
+            .padding()
+    }
+}
+
+struct MainWeatherStatusView: View {
+    var imageName: String
+    var temperature: Int
+    var body: some View {
+        Image(systemName: imageName)
+            .renderingMode(.original)
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 180, height: 180)
+        Text("\(temperature)°")
+            .font(.system(size: 70, weight: .medium, design: .default))
+            .foregroundColor(.white)
+    }
 }
--- WeatherApp/WeatherButton.swift
+++ WeatherApp/WeatherButton.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+import SwiftUI
+
+
+struct ButtonView: View {
+    var title: String
+    var textColor: Color
+    var backgroundColor: Color
+    var body: some View {
+        Button(action: {
+        }, label: {
+            Text(title)
+                .frame(width: 250, height: 50)
+                .foregroundColor(textColor)
+                .font(.system(size: 30, weight: .regular, design: .default))
+                .background(backgroundColor)
+                .cornerRadius(30)
+        })
+        .padding(.top, 30)
+    }
+} 

9335d0be3b1e53b000d4073baf8189e4f6952ede
Mironisegor <mironove55@icloud.com>
2024-04-04 16:39:45 +0300

    Working App

--- WeatherApp/WeatherButton.swift
+++ WeatherApp/WeatherButton.swift
@@ -5,24 +5,28 @@
 //  Created by Vladimir Fursov on 03.04.2024.
 //
 
-import Foundation
 import SwiftUI
 
-
-struct ButtonView: View {
+struct WeatherButton: View {
+    
     var title: String
     var textColor: Color
     var backgroundColor: Color
+    
     var body: some View {
-        Button(action: {
-        }, label: {
-            Text(title)
-                .frame(width: 250, height: 50)
-                .foregroundColor(textColor)
-                .font(.system(size: 30, weight: .regular, design: .default))
-                .background(backgroundColor)
-                .cornerRadius(30)
-        })
-        .padding(.top, 30)
+        Text(title)
+            .frame(width: 280, height: 50)
+            .background(backgroundColor.gradient)
+            .foregroundColor(textColor)
+            .font(.system(size: 20, weight: .bold))
+            .cornerRadius(10)
     }
-} 
+}
+
+struct WeatherButton_Previews: PreviewProvider {
+    static var previews: some View {
+        WeatherButton(title: "Test Title",
+                      textColor: .white,
+                      backgroundColor: .blue)
+    }
+}
